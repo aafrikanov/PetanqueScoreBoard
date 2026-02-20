4648eb3947cd83dc77aa336d34a0273ea61ab5ea
aafrikanov <afrikanovav@m2.ru>
2026-02-20 15:44:00 +0300

    Change color

--- src/pages/WidgetPage.tsx
+++ src/pages/WidgetPage.tsx
@@ -1,10 +1,10 @@
-import { useEffect } from 'react'
-import { useScore } from '../context/ScoreContext'
-import type { TeamScore } from '../types/score'
-import './WidgetPage.css'
+import { useEffect } from "react";
+import { useScore } from "../context/ScoreContext";
+import type { TeamScore } from "../types/score";
+import "./WidgetPage.css";
 
-const WIDGET_EMBED_CLASS = 'widget-embed'
-const WIDGET_POLL_INTERVAL_MS = 5000
+const WIDGET_EMBED_CLASS = "widget-embed";
+const WIDGET_POLL_INTERVAL_MS = 5000;
 
 function TeamRow({ team }: { team: TeamScore }) {
   return (
@@ -14,30 +14,34 @@ function TeamRow({ team }: { team: TeamScore }) {
         {[1, 2, 3, 4, 5, 6].map((i) => (
           <div
             key={i}
-            className={`widget-ball ${i <= team.balls ? 'active' : ''}`}
+            className={`widget-ball ${i <= team.balls ? "active" : ""}`}
             aria-hidden
           />
         ))}
       </div>
       <div className="widget-score">{team.score}</div>
     </div>
-  )
+  );
 }
 
 export function WidgetPage() {
-  const { score, refreshFromStorage } = useScore()
+  const { score, refreshFromStorage } = useScore();
   useEffect(() => {
-    document.body.classList.add(WIDGET_EMBED_CLASS)
-    return () => document.body.classList.remove(WIDGET_EMBED_CLASS)
-  }, [])
+    document.documentElement.classList.add(WIDGET_EMBED_CLASS);
+    document.body.classList.add(WIDGET_EMBED_CLASS);
+    return () => {
+      document.documentElement.classList.remove(WIDGET_EMBED_CLASS);
+      document.body.classList.remove(WIDGET_EMBED_CLASS);
+    };
+  }, []);
   useEffect(() => {
-    const id = setInterval(refreshFromStorage, WIDGET_POLL_INTERVAL_MS)
-    return () => clearInterval(id)
-  }, [refreshFromStorage])
+    const id = setInterval(refreshFromStorage, WIDGET_POLL_INTERVAL_MS);
+    return () => clearInterval(id);
+  }, [refreshFromStorage]);
   return (
     <div className="widget-panel">
       <TeamRow team={score.team1} />
       <TeamRow team={score.team2} />
     </div>
-  )
+  );
 }
